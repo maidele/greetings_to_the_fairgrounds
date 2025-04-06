6de94f60fbedbce327fff5503a74fe015ec3afca
Maidele Archelus <maidele@usf.edu>
2025-04-06 01:37:44 -0400

    project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-import Greetings from "./components/Greetings";
+import Greetings from "./components/Greeting";
+
 
 function App() {
-  return(
-    <div>
-      <h1> Welcome to our shop!</h1>
-      <Greetings name="Chloe Washington" />
-      <h2>Products</h2>
-        <Products name= "iPhone 13" price="$1000"/>
-        <Products name= "iPhone 13 Pro" price="$1100"/>
-        <Products name= "iPhone 13 Pro" price="$1200"/>
-    </div>
-  );
+ return(
+   <div>
+     <h1> Welcome to our shop!</h1>
+     <Greetings name="Chloe Washington" />
+     <h2>Products</h2>
+       <Products name= "iPhone 13" price="$1000"/>
+       <Products name= "iPhone 13 Pro" price="$1100"/>
+       <Products name= "iPhone 13 Pro" price="$1200"/>
+   </div>
+ );
 }
 export default App;
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 
+
 function Products({name,price}) {
-    return (
-        <div>
-            <h3> Products</h3>
-                <p> Product Name: {name}</p>
-                <p> Product Price: {price}</p>
-                <button> Add to Cart</button>
-                <button> Remove from Cart</button>
-                <button> Buy Now</button>
-        </div>
-    );
+   return (
+       <div>
+           <h3> Products</h3>
+               <p> Product Name: {name}</p>
+               <p> Product Price: {price}</p>
+               <button> Add to Cart</button>
+               <button> Remove from Cart</button>
+               <button> Buy Now</button>
+       </div>
+   );
 }
 export default Products;
 //
