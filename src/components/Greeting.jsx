794c92abb298bba0821b79e90aa2da61ddb829f5
Maidele Archelus <maidele@usf.edu>
2025-04-06 22:41:25 -0400

    updated code according to assignment details

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -4,7 +4,7 @@ import React from "react";
 function Greetings({name}) {
    return (
        <div>
-           <h3> Hello,{name}!</h3>
+           <h3> Hello,{name} from the Greeting Component!</h3>
            <p> We welcome you to discover our products that are below!This is your first custom component in React.</p>
        </div>
    );
